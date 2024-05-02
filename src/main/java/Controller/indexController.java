fad9c084dcb8342df811222c1a6f75342dcb3811
AliciaMayor <amayob01@estudiantes.unileon.es>
2024-05-02 17:53:14 +0200

    Mensaje error login y creadas varias vistas

--- src/main/java/Controller/indexController.java
+++ src/main/java/Controller/indexController.java
@@ -13,6 +13,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import org.primefaces.PrimeFaces;
 
 /**
  *
@@ -24,6 +25,7 @@ import javax.inject.Named;
 public class indexController implements Serializable{
     
     private User user;
+    private String errorLoginMessage;
     
     @EJB
     private UserFacadeLocal userEJB;
@@ -50,12 +52,21 @@ public class indexController implements Serializable{
         return userEJB;
     }
     
+    public String getErrorLoginMessage(){
+        return errorLoginMessage;
+    }
+    
+    public void setErrorLoginMessage(String errorLoginMessage) {
+        this.errorLoginMessage = errorLoginMessage;
+}
+    
     public String verifyUser(){
         
         /*El ?faces-redirect-true se añade para que mientras desarrollamos la app nos muestre a 
           donde estamos navegando, podemos quitarlo cuando esté lista. Es lo suyo quitarlas por 
           motivos de seguridad*/
         String direction = "public/insufficientPermissions.xhtml?faces-redirect=true";
+        //String direction = "index.xhtml?faces-redirect=true";
         
         User searchedUser = userEJB.verifyUser(user);
         
@@ -69,6 +80,10 @@ public class indexController implements Serializable{
                 direction = "private/mainViewUser.xhtml?faces-redirect=true";
             }
             
+        }else{
+            errorLoginMessage = "Nombre de usuario o contraseña incorrectos.";
+            //PrimeFaces.current().executeScript("PF('errorDialog').show()");
+                   
         }
         
         return direction;
