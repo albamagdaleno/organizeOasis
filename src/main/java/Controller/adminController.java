53ea8e0bd2bc22d63fe366d3f16c1c26af4bb74c
albamagdaleno <amagdr00@estudiantes.unileon.es>
2024-05-04 20:43:36 +0200

    Vista de Admin cn la tabla añadida (sin datos mostrados)

---
+++ src/main/java/Controller/adminController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author albamagdaleno
+ */
+@Named
+@ViewScoped
+
+public class adminController implements Serializable{
+    
+    
+}
